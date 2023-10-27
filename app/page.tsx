f01ffd1bc8e2b60d55843af07431db4c2324489f
Matthew Kwong <matthewkwong2@gmail.com>
2023-10-28 02:17:10 +0800

    feat: add MUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Button>test</Button>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
@@ -15,7 +17,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -91,5 +93,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.NODE_ENV === "production",
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withBundleAnalyzer(nextConfig);
